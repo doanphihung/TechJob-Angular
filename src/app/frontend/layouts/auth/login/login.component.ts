16fe2130df413ea503002a9c795f23f0aae4b6eb
ducbl98 <ductit1998@gmail.com>
2021-07-30 08:28:26 +0700

    login

--- src/app/frontend/layouts/auth/login/login.component.ts
+++ src/app/frontend/layouts/auth/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {Router} from "@angular/router";
+import {AuthService} from "../../../../share/services/auth.service";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  // @ts-ignore
+  formLogin:FormGroup;
+  constructor(private formBuilder: FormBuilder,
+              private router: Router,
+              private authService:AuthService,
+  ) {
+  }
 
   ngOnInit(): void {
+    this.formLogin= this.formBuilder.group({
+      'email':['',],
+      'password':['',],
+    })
+  }
+
+  login(){
+    const loginInfor= this.formLogin?.value;
+
   }
 
 }
